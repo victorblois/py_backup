41c6d2778209e4b5d61082606bfafb8f0bb21ae3
VICTOR BLOIS <vlmblois@yahoo.com.br>
2020-08-05 18:43:43 -0300

    Added firebird bkp scripts

--- notebook/bkpfdb.bat
+++ notebook/bkpfdb.bat
@@ -1,4 +1,3 @@
-REM c:\python27\python.exe C:\MyPhytonPrivado\MyNotes\notebook\file_bkp.py
 REM Restore
 REM "C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe" -c -user SYSDBA -password masterkey C:\Users\vlmbl\Dropbox\Baterias\Baterias.fbk C:\Users\vlmbl\Dropbox\Baterias\Baterias.fdb
 
@@ -8,17 +7,18 @@ REM "C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe" -c -user SYSDBA -passw
     if errorlevel 7 (
         echo NO - Sair
     ) else if errorlevel 6 (
-	
-        del F:\Sistema\Baterias_bkp.log >nul 2>&1
-		
+        
+		echo YES - Fazendo backup
+				
+		del F:\Sistema\Baterias_bkp.log >nul 2>&1
 		"C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe" -t -user SYSDBA -password masterkey -y F:\Sistema\Baterias_bkp.log F:\Sistema\BATERIAS.FDB F:\Sistema\BATERIAS.fbk
 		"C:\Program Files\7-Zip\7z.exe" a -tzip F:\Sistema\BATERIAS.zip F:\Sistema\BATERIAS.fbk
-		
 		echo f | xcopy /y F:\Sistema\BATERIAS.zip C:\Users\vlmbl\Dropbox\Baterias\Baterias.zip
-		echo f | xcopy /y F:\Sistema\BATERIAS.fbk C:\Users\vlmbl\Dropbox\Baterias\Baterias.fbk	
+		echo f | xcopy /y F:\Sistema\BATERIAS.fbk C:\Users\vlmbl\Dropbox\Baterias\Baterias.fbk
 		
-		C:\ProgramData\Anaconda3\python.exe C:\MyGit\py_backup\notebook\file_bkp.py				
-
+		echo msgbox "Backup feito com sucesso!" > %tmp%\tmp.vbs
+		wscript %tmp%\tmp.vbs
+		del %tmp%\tmp.vbs
 	)
 
     exit /b
@@ -30,5 +30,8 @@ REM "C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe" -c -user SYSDBA -passw
     set "exitCode=%errorlevel%" & del "%tempFile%" >nul 2>nul
     endlocal & exit /b %exitCode%
 	
+	
+
+
 
 
--- notebook/readme.txt
+++ notebook/readme.txt
@@ -1,6 +1,14 @@
 -----------------------------------------
-1. bkp.bat
+1. bkpfdb.bat
 -----------------------------------------
+Utiliza  o utilitario gbak para fazer o backup
+
+-----------------------------------------
+1.1 bkp.bat
+-----------------------------------------
+Não é backup de banco e sim uma cópia do arquivo de banco. 
+Cuidado se o banco estiver aberto ou fazendo transações.
+
 Colocar endereço do Python e caminho de file_bkp.py
 Ex.:
 C:\ProgramData\Anaconda3\python.exe C:\MyGit\py_backup\notebook\file_bkp.py
